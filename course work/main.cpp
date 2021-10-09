bcce16356f5f82332cdc45d3d002d8bb121bdba4
IlyaGrytsatsuev <ilya.gr2003@yandex.ru>
2021-10-09 22:26:04 +0300

    Initial Commit

---
+++ course work/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  course work
+//
+//  Created by Илья Грицацуев on 09.10.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
